aea497ade87edd52b6b79e1ac3aecf01cda6bb34
flow-pie <startabase@gmail.com>
2026-02-07 20:47:52 +0000

    Chore: Update project-level URL dispatcher to integrate modular user URL submodules

--- tyrent_backend/urls.py
+++ tyrent_backend/urls.py
@@ -3,7 +3,13 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('api/auth/', include('users.urls_auth')),
+    path('api/admin/', include('users.urls_admin')),
+    path('api/users', include('users.urls_users')),
+    path('api/landlord/', include('users.urls_landlord')),
+    path('api/tenant/', include('users.urls_tenant')),
+
+
     path('properties/', include('properties.urls')),
     path('wallet/', include('wallet.urls')),
     path('bookings/', include('bookings.urls')),
